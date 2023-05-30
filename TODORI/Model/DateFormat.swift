e70a9afce2ea86fc5078aefc9d875ec98860829c
Dasol <solda@Dasols-Laptop.local>
2023-05-31 08:23:41 +0900

    updated

--- TODORI/Model/DateFormat.swift
+++ TODORI/Model/DateFormat.swift
@@ -2,10 +2,9 @@
 //  DateFormat.swift
 //  TODORI
 //
-//  Created by 제임스 on 2023/04/24.
+//  Created by Dasol on 2023/05/31.
 //
 
-import Foundation
 import UIKit
 
 class DateFormat{
@@ -27,7 +26,7 @@ class DateFormat{
     
     func getDay(date:Date) -> String{
         dateFormatter.locale = Locale(identifier: "ko")
-        dateFormatter.dateFormat = "MM"
+        dateFormatter.dateFormat = "dd"
         return dateFormatter.string(from: date)
     }
     
@@ -38,6 +37,34 @@ class DateFormat{
         return arr
     }
     
+    func getYearMonthDayAndWeekday(date:Date) -> String {
+        dateFormatter.locale = Locale(identifier: "ko")
+        dateFormatter.dateFormat = "MM월 dd E요일"
+        let date = dateFormatter.string(from: date)
+        return date
+    }
+    
+    func isWeekend(date:Date) -> Bool{
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.dateFormat = "E"
+        let weekday = dateFormatter.string(from: date)
+        if weekday == "Sat" || weekday == "Sun"{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func getHour(date:Date) -> String{
+        dateFormatter.locale = Locale(identifier: "ko")
+        dateFormatter.dateFormat = "HH"
+        let date = dateFormatter.string(from: date)
+        return date
+    }
+    
+    func getMinute(date:Date) -> String{
+        dateFormatter.locale = Locale(identifier: "ko")
+        dateFormatter.dateFormat = "mm"
+        let date = dateFormatter.string(from: date)
+        return date    }
 }
-
-
